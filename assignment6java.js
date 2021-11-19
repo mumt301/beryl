b6d09458f5cf6c4450730fe355d658603c6c6c85
Beryl Arnould <Beryl@Beryls-MacBook-Air.local>
2021-11-19 14:29:18 -0500

    assignment 6

--- assignment6java.js
+++ assignment6java.js
@@ -1,24 +1,16 @@
 function queryArtist() {
-    // Step 2: Parse the data submitted through the form 
-    // In other words, get the parameters from the URL (@name = 'artist')let params = (new URL(document.location)).searchParams;
+    // Step 2: Parse the data submitted through the form, get the parameters from the URL (@name = 'artist')
     let params = (new URL(document.location)).searchParams;
     if (params.has('artist')) {
         let artistName = params.get('artist');
         console.log(artistName);
-        
         // Step 3: Craft the MusicBrainz call/message for requesting information about the artist
-        // GET request --> type 'search'
         let mbBaseURL = "https://musicbrainz.org/ws/2/";
         let mbResource = "artist?query=";
         let queryURL = mbBaseURL + mbResource + artistName;
         console.log(queryURL);
-        
         // Step 4–6
         httpGet(queryURL, getMBID);
-        // Step 4: Make the HTTP request call to MusicBrainz and wait for its response
-        // When the response is ready, it will use the callback function getMBID().
-        // getMBID() will perform steps 5 and 6, parsing the response of the server
-        // and rendering the results in the browser.
     }
 }
 
@@ -31,26 +23,19 @@ function queryArtist() {
 // Step 4: Make the HTTP request call to MusicBrainz
 function httpGet(theURL, cbFunction) {
     let xmlHttp = new XMLHttpRequest(); // Creates a new XMLHttpRequest object
-    
-    // Specify the request method and URL of the request, and send the request to the server
-    xmlHttp.open("GET", theURL);
-    xmlHttp.send();
-    
-    // Wait for the response to be ready and store it in a variable
+    xmlHttp.open("GET", theURL); // Specifies the request method and URL of the request
+    xmlHttp.send(); // Sends the request to the server
+    // Callback function
     xmlHttp.onreadystatechange = function () {
-        // Define a function to be called when the readyState property changes
         if (this.readyState == 4 && this.status == 200) {
-            // when readyState is 4 and status is 200, the response is ready
-            // Then, we call the callback function to parse the response and render the appropriate results
             cbFunction(this);
         }
     };
 }
 
 function getMBID(xhttp) {
-    let retrievedData = xhttp.responseXML; // Returns the response as XML data
+    let retrievedData = xhttp.responseXML;
     console.log(retrievedData);
-    
     // Step 5: Parse the response from the server
     let artistData = retrievedData.getElementsByTagName('artist')[0];
     console.log(artistData);
@@ -58,56 +43,63 @@ function getMBID(xhttp) {
     console.log(artistName);
     let artistMBID = artistData.id;
     console.log(artistMBID);
-    let mBaseURL = "https://musicbrainz.org/ws/2/release-group?artist=";
+    let mBaseURL = "https://musicbrainz.org/ws/2/release-group?artist="; // Defining a new base URL
     let queryURL = mBaseURL + artistMBID;
     console.log(queryURL);
     httpGet(queryURL, getAlbums)
 }
 
+// Function to get artist's discography
 function getAlbums(xhttp) {
-    let retrievedData = xhttp.responseXML; // Returns the response as XML data
+    let retrievedData = xhttp.responseXML;
     console.log(retrievedData);
-    var albums = [];
-    let discography=retrievedData.getElementsByTagName('release-group');
-    // get album only, not all released-groups
-    for (var i = discography.length - 1; i >=0; i--) {
-        let type=discography[i].getAttribute('type');
+    var discography = []; // Creating a variable to store artist's discography
+    let albums=retrievedData.getElementsByTagName('release-group');
+    // For loop to add data into discography variable (adding album only, not all released-groups)
+    for (var i = albums.length - 1; i >=0; i--) {
+        let type=albums[i].getAttribute('type');
         console.log(type);
         if(type=='Album') {
-            albums.push(discography[i]);
+            discography.push(albums[i]);
         }
     }
-    sortedAlbums=albums.sort((a,b)=>new Date(a.getElementsByTagName('first-release-date')[0].innerHTML) - new Date(b.getElementsByTagName('first-release-date')[0].innerHTML));
-    displayTable(sortedAlbums);
-    //console.log(sortedAlbums);
-
+    // Creating a new variable with sorted discography data by date (oldest to newest)
+    sortedDisco=discography.sort((a,b)=>new Date(a.getElementsByTagName('first-release-date')[0].innerHTML) - new Date(b.getElementsByTagName('first-release-date')[0].innerHTML));
+    displayTable(sortedDisco);
 }
 
-function displayTable(albums) {
+// Function to display discography data in a table
+function displayTable(discography) {
+    // Creating a table tag
     let table = document.createElement('table');
     table.id = 'table';
+    // Creating table row and table data tags 
     tr = document.createElement('tr');
     td1 = document.createElement('td');
     td2 = document.createElement('td');
-    tr.appendChild(td1);
+    // Adding data (table headers) inside respective elements
     td1.innerHTML = "<b>Album Name<b>";
-    tr.appendChild(td2);
     td2.innerHTML = "<b>Date of Release<b>";
+    // Appending data to its respective rows and table
+    tr.appendChild(td1);
+    tr.appendChild(td2);
     table.appendChild(tr);
     
-    for (row = 0; row < albums.length; row++) {
+    // For loop to add album title/name and release date to table
+    for (row = 0; row < discography.length; row++) {
         tr = document.createElement('tr');
         td1 = document.createElement('td');
-        tr.appendChild(td1);
-        td1.innerHTML = albums[row].getElementsByTagName('title')[0].innerHTML;
         td2 = document.createElement('td');
+        td1.innerHTML = discography[row].getElementsByTagName('title')[0].innerHTML;
+        td2.innerHTML = discography[row].getElementsByTagName('first-release-date')[0].innerHTML;
+        tr.appendChild(td1);
         tr.appendChild(td2);
-        td2.innerHTML = albums[row].getElementsByTagName('first-release-date')[0].innerHTML;
         table.appendChild(tr);
     }
     
+    // Appending the table to the divider with id='table'
     document.getElementById('table').appendChild(table);
     }
 
-// Call to the main function at loading
+// Calling the main function at loading
 window.onload = queryArtist;
